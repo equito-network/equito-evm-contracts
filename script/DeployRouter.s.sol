cd133b757fd75f361d468508b31a5afb9c1b5f91
tesol2y090 <gliese090@gmail.com>
2024-04-11 23:42:02 +0700

    feat: add router deploy script

---
+++ script/DeployRouter.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.23;
+
+import "forge-std/StdJson.sol";
+import "forge-std/Script.sol";
+
+import {Router} from "../src/Router.sol";
+
+contract DeployRouter is Script {
+    using stdJson for string;
+
+    uint256 public deployPrivateKey = vm.envUint("PRIVATE_KEY_DEPLOY");
+    uint256 public chainSelector = vm.envUint("CHAIN_SELECTOR");
+    address public deployerAddress = vm.rememberKey(deployPrivateKey);
+    string public outputFilename = vm.envString("OUTPUT_FILENAME");
+
+    Router public router;
+
+    function run() public {
+        string memory root = vm.projectRoot();
+        string memory basePath = string.concat(root, "/script/deployment/");
+        string memory path = string.concat(basePath, "Router-");
+        // start broadcasting transactions
+        vm.startBroadcast(deployerAddress);
+
+        console.log("======== Deploying Router =========");
+        router = new Router(chainSelector);
+        console.log("Deployed Router successfully =>", address(router));
+
+        console.log("======== Finished deploy process =========");
+
+        // finish broadcasting transactions
+        vm.stopBroadcast();
+
+        // Write to file
+        path = string.concat(path, outputFilename);
+        vm.writeJson(
+            vm.serializeAddress("contracts", "router", address(router)),
+            path
+        );
+    }
+}
