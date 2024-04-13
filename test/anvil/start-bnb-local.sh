fbadef62ec21348cfa357306fb608b1c6e7b8eb4
tesol2y090 <gliese090@gmail.com>
2024-04-14 00:45:01 +0700

    feat: add local genesis chain config

--- test/anvil/start-bnb-local.sh
+++ test/anvil/start-bnb-local.sh
@@ -5,4 +5,4 @@ RPC_URL=http://localhost:8546
 CHAIN_ID=97
 PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
-anvil --port $PORT --chain-id $CHAIN_ID
+anvil --port $PORT --chain-id $CHAIN_ID --init $(pwd)/test/config/bnb-genesis.json
--- test/anvil/start-sepolia-local.sh
+++ test/anvil/start-sepolia-local.sh
@@ -5,6 +5,6 @@ RPC_URL=http://localhost:8545
 CHAIN_ID=11155111
 PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
-anvil --port $PORT --chain-id $CHAIN_ID
+anvil --port $PORT --chain-id $CHAIN_ID --init $(pwd)/test/config/sepolia-genesis.json
 
 # forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY --constructor-args $CHAIN_ID src/Router.sol:Router
