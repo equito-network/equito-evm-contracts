f8500b8025ecf7a70c3d60d8e38f300370715ce5
Matteo Muraca <mmuraca247@gmail.com>
2024-04-16 18:54:34 +0200

    `IEquitoVerifier` interface

--- src/interfaces/IEquitoVerifier.sol
+++ src/interfaces/IEquitoVerifier.sol
@@ -2,7 +2,11 @@
 
 pragma solidity ^0.8.23;
 
+<<<<<<< HEAD
 import {EquitoMessage} from "../libraries/EquitoMessageLibrary.sol";
+=======
+import {EquitoMessage} from "../libraries/EquitoMessage.sol";
+>>>>>>> dbffd6a (`IEquitoVerifier` interface)
 
 interface IEquitoVerifier {
     function verifyMessages(EquitoMessage[] calldata messages, bytes calldata proof) external; 
