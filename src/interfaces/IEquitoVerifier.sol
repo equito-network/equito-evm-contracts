b082932ba0bf2680cda8f8a439d3e7281ffc55cb
tesol2y090 <gliese090@gmail.com>
2024-04-20 16:25:54 +0700

    merge conflict

--- src/interfaces/IEquitoVerifier.sol
+++ src/interfaces/IEquitoVerifier.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.23;
 
 import {EquitoMessage} from "../libraries/EquitoMessage.sol";
 
 interface IEquitoVerifier {
-    function verifyMessages(EquitoMessage[] calldata messages, bytes calldata proof) external; 
+    function verifyMessages(
+        EquitoMessage.EquitoMessage[] calldata messages,
+        bytes calldata proof
+    ) external;
 }
