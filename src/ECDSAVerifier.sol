7edacb747d72ab305f506793dd38dc680c606e9d
tesol2y090 <gliese090@gmail.com>
2024-04-22 03:04:24 +0700

    feat: add verify message logic

--- src/ECDSAVerifier.sol
+++ src/ECDSAVerifier.sol
@@ -3,12 +3,34 @@ pragma solidity ^0.8.23;
 
 import {IEquitoVerifier} from "./interfaces/IEquitoVerifier.sol";
 import {EquitoMessageLibrary} from "./libraries/EquitoMessageLibrary.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract ECDSAVerifier is IEquitoVerifier {
+    address[] public validatorAddress;
+    uint256 public immutable threshold = 70;
+
+    mapping(bytes32 => uint256) public messageToSignaturesCount;
+
+    constructor(address[] memory _validatorAddress) {
+        validatorAddress = _validatorAddress;
+    }
+
     function verifyMessages(
-        EquitoMessageLibrary.EquitoMessage[] calldata messages,
-        bytes calldata proof
+        EquitoMessageLibrary.EquitoMessage calldata messages,
+        bytes calldata signature
     ) external override {
-        // Verify the messages
+        bytes32 messageHash = EquitoMessageLibrary._hash(messages);
+
+        if (
+            (messageToSignaturesCount[messageHash] * 100) /
+                validatorAddress.length >=
+            threshold
+        ) revert MessageAlreadyVerified(messageHash);
+
+        for (uint256 i = 0; i < validatorAddress.length; i++) {
+            if (ECDSA.recover(messageHash, signature) == validatorAddress[i]) {
+                messageToSignaturesCount[messageHash]++;
+            }
+        }
     }
 }
--- src/interfaces/IEquitoVerifier.sol
+++ src/interfaces/IEquitoVerifier.sol
@@ -4,8 +4,15 @@ pragma solidity ^0.8.23;
 import {EquitoMessageLibrary} from "../libraries/EquitoMessageLibrary.sol";
 
 interface IEquitoVerifier {
+    error MessageAlreadyVerified(bytes32 messageHash);
+
+    event MessageVerified(
+        EquitoMessageLibrary.EquitoMessage message,
+        address validator
+    );
+
     function verifyMessages(
-        EquitoMessageLibrary.EquitoMessage[] calldata messages,
-        bytes calldata proof
+        EquitoMessageLibrary.EquitoMessage calldata messages,
+        bytes calldata signature
     ) external;
 }
