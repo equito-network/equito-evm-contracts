85f40da69041b9d2b6a96571504a36306f8019f3
Matteo Muraca <mmuraca247@gmail.com>
2024-04-26 09:54:47 +0200

    improved tests and mocks

--- test/Router.t.sol
+++ test/Router.t.sol
@@ -6,20 +6,17 @@ import {Test, console} from "forge-std/Test.sol";
 import {Router, IRouter} from "../src/Router.sol";
 import {EquitoMessage, EquitoMessageLibrary} from "../src/libraries/EquitoMessageLibrary.sol";
 import {MockReceiver} from "./mock/MockReceiver.sol";
-import {ECDSAVerifier} from "../src/ECDSAVerifier.sol";
+import {MockVerifier} from "./mock/MockVerifier.sol";
 
 contract RouterTest is Test {
-    ECDSAVerifier verifier;
+    MockVerifier verifier;
     Router router;
     MockReceiver receiver;
 
     address alice = address(0xA11CE);
-    address bob = address(0xB0B);
-
-    address[] validators = [alice, bob];
 
     function setUp() public {
-        verifier = new ECDSAVerifier(validators);
+        verifier = new MockVerifier();
         router = new Router(1, address(verifier));
         receiver = new MockReceiver();
     }
@@ -59,31 +56,47 @@ contract RouterTest is Test {
             memory messages = new EquitoMessage[](1);
         messages[0] = message;
 
-        router.routeMessages(messages);
+        router.routeMessages(messages, 0, abi.encode(1));
     }
 
     function testRouteMessagesWithDuplicateMessage() public {
         vm.prank(alice);
-        bytes memory data = abi.encode("Hello, World!");
+        bytes memory data1 = abi.encode("Hello, World!");
 
-        EquitoMessage memory message = EquitoMessage({
+        EquitoMessage memory message1 = EquitoMessage({
             blockNumber: 1,
             sourceChainSelector: 1,
             sender: abi.encode(alice),
             destinationChainSelector: 2,
             receiver: abi.encode(receiver),
-            data: data
+            data: data1
+        });
+
+        bytes memory data2 = abi.encode("This is a test!");
+
+        EquitoMessage memory message2 = EquitoMessage({
+            blockNumber: 1,
+            sourceChainSelector: 1,
+            sender: abi.encode(alice),
+            destinationChainSelector: 2,
+            receiver: abi.encode(receiver),
+            data: data2
         });
 
         EquitoMessage[]
-            memory messages = new EquitoMessage[](2);
-        messages[0] = message;
-        messages[1] = message;
+            memory messages = new EquitoMessage[](3);
+        messages[0] = message1;
+        messages[1] = message2;
+        messages[2] = message1;
 
-        bytes32 newMessage = router.sendMessage(abi.encode(receiver), 2, data);
+        bytes32 message1Hash = EquitoMessageLibrary._hash(message1);
+        bytes32 message2Hash = EquitoMessageLibrary._hash(message2);
 
-        router.routeMessages(messages);
+        router.routeMessages(messages, 0, abi.encode(1));
 
-        assertEq(router.isDuplicateMessage(newMessage), true);
+        assertEq(router.isDuplicateMessage(message1Hash), true);
+        assertEq(router.isDuplicateMessage(message2Hash), true);
+
+        assertEq(receiver.getMessage().data, message2.data);
     }
 }
---
+++ test/mock/MockVerifier.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.23;
+
+import {EquitoMessage} from "../../src/libraries/EquitoMessageLibrary.sol";
+import {IEquitoVerifier} from "../../src/interfaces/IEquitoVerifier.sol";
+
+/// Mock Verifier that returns true for all non-empty proofs.
+contract MockVerifier is IEquitoVerifier {
+    function verifyMessages(
+        EquitoMessage[] calldata messages,
+        bytes calldata proof
+    ) external override returns (bool) {
+        return proof.length > 0;
+    }
+
+    function verifySignatures(
+        bytes32 hash,
+        bytes calldata proof
+    ) external override returns (bool) {
+        return proof.length > 0;
+    }
+}
