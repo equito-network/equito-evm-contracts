983451fe95a8e52ebe8694cdb0557abe8657475d
Eugene Voronov <eugene@hmt.ai>
2024-05-20 10:30:09 +0300

    Primarily refoctored EquitoApp and Router contracts

--- src/EquitoApp.sol
+++ src/EquitoApp.sol
@@ -5,42 +5,56 @@ pragma solidity ^0.8.23;
 import {IRouter} from "./interfaces/IRouter.sol";
 import {IEquitoReceiver} from "./interfaces/IEquitoReceiver.sol";
 import {EquitoMessage} from "./libraries/EquitoMessageLibrary.sol";
+import {Errors} from "./libraries/Errors.sol";
 
-/// This abstract contract is the base for all applications that want to leverage 
+/// @title EquitoApp
+/// @notice This abstract contract is the base for all applications that want to leverage 
 /// the Equito cross-chain messaging protocol to communicate with other blockchains.
 abstract contract EquitoApp is IEquitoReceiver {
-    error InvalidRouter(address router);
-  
-    /// The Router Contract that is used to send and receive messages.
+    /// @dev The Router Contract that is used to send and receive messages.
     IRouter internal immutable router;
 
+    /// @notice Initializes the EquitoApp contract and set the router address.
+    /// @param _router The address of the router contract.
     constructor(address _router) {
+        if (_router == address(0)) {
+            revert Errors.RouterAddressCannotBeZero();
+        }
         router = IRouter(_router);
     }
 
-    /// Only the Router Contract is allowed to call the functions with this modifier.
+    /// @notice Modifier to restrict access to only the router contract.
     modifier onlyRouter() {
-        if (msg.sender != address(router)) revert InvalidRouter(msg.sender);
+        if (msg.sender != address(router)) revert Errors.InvalidRouter(msg.sender);
         _;
     }
 
-    /// Send a cross-chain message using Equito.
+    /// @notice Sends a cross-chain message using Equito.
+    /// @param receiver The address of the receiver.
+    /// @param destinationChainSelector The identifier of the destination chain.
+    /// @param data The message data.
+    /// @return The message ID.
     function sendMessage(
         bytes calldata receiver,
         uint256 destinationChainSelector,
         bytes calldata data
     ) external returns (bytes32) {
-        return router.sendMessage(receiver, destinationChainSelector, data);
+        bytes32 messageId = router.sendMessage(receiver, destinationChainSelector, data);
+        emit MessageSent(receiver, destinationChainSelector, data);
+        return messageId;
     }
 
-    /// Receive a cross-chain message from the Router Contract.
+    /// @notice Receives a cross-chain message from the Router Contract.
     /// It is a wrapper function for the `_receiveMessage` function, that needs to be overridden.
     /// Only the Router Contract is allowed to call this function.
+    /// @param message The Equito message received.
     function receiveMessage(EquitoMessage calldata message) external override onlyRouter {
+        emit MessageReceived(msg.sender, message.data);
         _receiveMessage(message);
     }
 
-    /// The actual logic for receiving a cross-chain message from the Router Contract
+    /// @notice The actual logic for receiving a cross-chain message from the Router Contract
     /// needs to be implemented in this function.
+    /// @param message The Equito message received.
     function _receiveMessage(EquitoMessage calldata message) internal virtual;
 }
--- src/interfaces/IRouter.sol
+++ src/interfaces/IRouter.sol
@@ -4,24 +4,42 @@ pragma solidity ^0.8.23;
 
 import {EquitoMessage} from "../libraries/EquitoMessageLibrary.sol";
 
+// Interface for the Router contract, used to interact with cross-chain messaging protocol
 interface IRouter {
-    event MessageSendRequested(
-        address indexed sender,
-        EquitoMessage data
-    );
+    /// @notice Emitted when a message send request is created.
+    /// @param sender The sender of the message.
+    /// @param message The message being sent.
+    event MessageSendRequested(address indexed sender, EquitoMessage message);
+
+    /// @notice Emitted when messages are delivered to their destination.
+    /// @param messages The list of messages that were delivered.
     event MessageSendDelivered(EquitoMessage[] messages);
 
+    /// @notice Emitted when a new verifier is added.
+    /// @param verifier The address of the new verifier.
+    event VerifierAdded(address indexed verifier);
+
+    /// @notice Sends a cross-chain message using Equito.
+    /// @param receiver The address of the receiver.
+    /// @param destinationChainSelector The chain selector of the destination chain.
+    /// @param data The message data.
+    /// @return The hash of the message.
     function sendMessage(
         bytes calldata receiver,
         uint256 destinationChainSelector,
         bytes calldata data
     ) external returns (bytes32);
 
+    /// @notice Routes messages to the appropriate receiver contracts.
+    /// @param messages The list of messages to be routed.
+    /// @param verifierIndex The index of the verifier used to verify the messages.
+    /// @param proof The proof provided by the verifier.
     function routeMessages(
         EquitoMessage[] calldata messages,
         uint256 verifierIndex,
         bytes calldata proof
     ) external;
 
+    /// @notice Returns the chain selector of the current chain.
     function chainSelector() external view returns (uint256);
 }
